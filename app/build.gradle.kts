3322cd0fc155247bf753a65c62a9144d46dcafe8
jksalcedo <jks.create@gmail.com>
2025-09-29 12:14:16 +0000

    Update minimum SDK version to 26
    This commit updates the `minSdk` version in the `app/build.gradle.kts` file from 23 to 26.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.jksalcedo.passvault"
-        minSdk = 23
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
