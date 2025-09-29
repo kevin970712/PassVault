7cbfcd642171d2ce5eb33742a308d67135225d74
jksalcedo <jks.create@gmail.com>
2025-09-29 14:36:19 +0000

    Refactor PasswordGenerator to allow custom character sets
    This commit updates the `PasswordGenerator` to provide more control over the generated passwords by allowing users to specify whether to include uppercase letters, lowercase letters, numbers, and symbols.

--- app/src/main/java/com/jksalcedo/passvault/utils/PasswordGenerator.kt
+++ app/src/main/java/com/jksalcedo/passvault/utils/PasswordGenerator.kt
@@ -1,8 +1,36 @@
 package com.jksalcedo.passvault.utils
 
 object PasswordGenerator {
-    fun generate(length: Int = 16): String {
-        val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#\$%^&*()"
+    fun generate(
+        length: Int = 16,
+        hasUppercase: Boolean = false,
+        hasLowercase: Boolean = false,
+        hasNumber: Boolean = false,
+        hasSymbols: Boolean = false
+    ): String {
+        val chars = StringBuilder()
+        val uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        val lowercase = "abcdefghijklmnopqrstuvwxyz"
+        val numbers = "0123456789"
+        val symbols = "!@#\\/$%^&*()"
+
+        if (hasUppercase) {
+            chars.append(uppercase)
+        }
+        if (hasLowercase) {
+            chars.append(lowercase)
+        }
+        if (hasNumber) {
+            chars.append(numbers)
+        }
+        if (hasSymbols) {
+            chars.append(symbols)
+        }
+
+        val selections = hasUppercase && hasLowercase && hasNumber && hasSymbols
+
+        if (!selections) !selections
+
         return (1..length).map { chars.random() }.joinToString("")
     }
 }
