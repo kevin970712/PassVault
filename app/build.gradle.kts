b79e88438416e8f16ae01b54ffeb91c08de27d71
jksalcedo <jks.create@gmail.com>
2025-11-15 06:03:52 +0000

    Update dependencies and app version
    This commit updates several dependencies and the app version.
    
    - Android Gradle Plugin: 8.13.0 -> 8.13.1
    - Navigation KTX: 2.9.5 -> 2.9.6
    - App version: 0.4-beta -> 0.4-beta2 (versionCode 5 -> 6)
    
    Additionally, the project's JDK configuration in IntelliJ IDEA has been updated from `temurin-21` to `jbr-21`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.jksalcedo.passvault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4-beta"
+        versionCode = 6
+        versionName = "0.4-beta2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
