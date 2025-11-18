8550f4300230ae4c70a3a164102be37725b5aa4d
jksalcedo <jks.create@gmail.com>
2025-11-18 11:53:18 +0000

    chore: Bump version to 0.6.0
    Updates the application version from `0.6.0-beta2` to `0.6.0` and increments the version code from 8 to 9, signifying a stable release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.jksalcedo.passvault"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.6.0-beta2"
+        versionCode = 9
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
